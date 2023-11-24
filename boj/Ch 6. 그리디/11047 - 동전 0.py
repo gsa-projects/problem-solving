0718345f2c713ea6a5e598819e15130f90bb8161
Hyunseung Ryu <rhseungg@gmail.com>
2023-11-24 14:49:10 +0900

    그리디

---
+++ boj/Ch 6. 그리디/11047 - 동전 0.py
@@ -0,0 +1,21 @@
+__problem__ = 'https://boj.kr/11047', '동전 0'
+
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+N = [int(input().strip()) for _ in range(n)]
+
+count = 0
+for e in reversed(N):
+    # t = k // e
+    # count += t
+    # k -= e * t
+
+    q, k = divmod(k, e)
+    count += q
+
+    if k == 0:
+        print(count)
+        exit()
---
+++ boj/Ch 6. 그리디/1715 - 카드 정렬하기.py
@@ -0,0 +1,21 @@
+__problem__ = 'https://boj.kr/1715', '카드 정렬하기'
+
+import sys
+from heapq import heappop, heappush
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+N = []
+
+for _ in range(n):
+    heappush(N, int(input().strip()))
+
+s = 0
+# todo: 니가 다시 풀어
+while len(N) > 1:
+    t = heappop(N) + heappop(N)
+    heappush(N, t)
+    s += t
+
+print(s)
