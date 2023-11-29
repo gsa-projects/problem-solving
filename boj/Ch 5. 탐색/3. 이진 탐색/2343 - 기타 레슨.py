9f71617487efe1c8276559d14a498a9af0fc4e69
Hyunseung Ryu <rhseungg@gmail.com>
2023-11-29 22:53:54 +0900

    다익스트라 완료

--- boj/Ch 5. 탐색/3. 이진 탐색/2343 - 기타 레슨.py
+++ boj/Ch 5. 탐색/3. 이진 탐색/2343 - 기타 레슨.py
@@ -9,7 +9,7 @@ L = list(map(int, input().split()))
 
 # 한 blueray당 capacity를 건네주면 총 blueray 개수를 반환하는 함수
 def get_count_of_blueray(L, capacity):
-    s = 0
+    s = 0   
     count = 1
 
     for l in L:
@@ -29,9 +29,9 @@ while i <= j:
     mid = (i + j) // 2
     count = get_count_of_blueray(L, mid)
 
-    if count > mid:
+    if count > m:
         i = mid + 1
-    elif count <= mid:
+    elif count <= m:
         result = mid
         j = mid - 1
 
